bd59f41afc89b8be274e3f437ab0f05bdb4d3339
isaprestess <164901803+isaprestess@users.noreply.github.com>
2024-10-28 12:04:33 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,48 @@
+criaCartao(
+    'Quem eu sou?',
+    '',
+    'Eu sou a isa, tenho 16 anos, gosto de jogar volêi e sou cristã'
+)
+
+criaCartao(
+    'minha família é',
+    '',
+    'Complicada, mas amorosa'
+)
+
+criaCartao(
+    'Onde moro?',
+    '',
+    'Moro em Pinhão-PR, no bairro Vila Caldas, longitude -25.707320, latitude -51.645064 '
+)
+
+criaCartao(
+    'Onde eu moro?',
+    '',
+    'No colégio estadual Mario Evaldo Morski, 1979 foi fundado'
+) 
+
+criaCartao(
+    'Minha turma',
+    '',
+    'É cheia de alunos inteligentes e muitos também desinteressados'
+)
+
+criaCartao(
+    'Faculdade?',
+    '',
+    'Não sei mais qual quero fazer'
+) 
+criaCartao(
+    'Minha cidade?',
+    '',
+    'É pequena mais eu gosto'
+) 
+
+criaCartao(
+    'Meu sonho?',
+    '',
+    'Sonho em ter uma casa confortável, e um trabalho bom'
+) 
+
+
